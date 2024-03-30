6d12017776e5dba37d480808f8db4b125194745a
Angelin Lalev <lalev@uni-svishtov.bg>
2024-03-30 17:11:13 +0200

    Working on gbm data preparation

--- prepare_gbm.py
+++ prepare_gbm.py
@@ -29,7 +29,7 @@ from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
 import statsmodels.graphics.tsaplots as tsa
 import random as rnd
-
+from dateutil.relativedelta import relativedelta 
 
 ### TUNING & CONFIG
 
@@ -37,17 +37,17 @@ import random as rnd
 # This option will cause computing lags for each variable in the data.
 # These lags will be fed into the LSTM model, toghether with the 
 # original variables. 
-hourly_lookback = 72           # Hourly data for three days
-daily_lookback = 7             # Daily aggregated data for 6 days back
-weekly_lookback = 52           # Weekly aggregated data for a year
-
+hourly_lookback = 24           # Hourly data for pollution from n past hours 
+daily_lookback = 7             # Daily aggregated data from n past days 
+weekly_lookback = 53           # Weekly aggregated data for n past weeks 
+monthly_lookback = 12          # Monthly aggregated data for n past months 
 
 ### CONSTANTS 
 
-input_data_loc = os.path.join("original_data", "airpollution", "LSTM-Multivariate_pollution.csv")
+input_data_loc = os.path.join("original_data", "airpollution", "pollution.csv")
 # The random component is so we don't overwrite our transformations each 
 # time we tune the process
-output_data_loc = os.path.join("transformed_data", "lstm_input-%d.csv"%rnd.randint(0, 1000))
+output_data_loc = os.path.join("transformed_data", "gbm_input-%d.csv"%rnd.randint(0, 1000))
 
 ### INITIALIZATION
 
@@ -113,11 +113,11 @@ for i in range(1, hourly_lookback):
 
 ## Daily and monthly
 
-data['month']= pd.to_datetime(data['date']).dt.month
-data['year']=pd.to_datetime(data['date']).dt.year
-data['day']=pd.to_datetime(data['date']).dt.day
-data['week']=pd.to_datetime(data['date']).dt.isocalendar().week                
-
+data.insert(1, 'year', pd.to_datetime(data['date']).dt.year)
+data.insert(2, 'month', pd.to_datetime(data['date']).dt.month)
+data.insert(3, 'day', pd.to_datetime(data['date']).dt.day)
+data.insert(4, 'week', pd.to_datetime(data['date']).dt.isocalendar().week)                
+data.insert(5, 'weekday', pd.to_datetime(data['date']).dt.weekday)
 
 daily_average_pollution = data.groupby(['year', 'month', 'day'])['pollution'].transform('mean')
 data['d_avg_pollution']=daily_average_pollution
@@ -125,14 +125,31 @@ data['d_avg_pollution']=daily_average_pollution
 weekly_average_pollution = data.groupby(['year', 'week'])['pollution'].transform("mean")
 data['w_avg_pollution'] = weekly_average_pollution
 
-
 for i in range(1, daily_lookback):
     data['d_avg_pollution-%d' % i] = data['d_avg_pollution'].shift(i*24)
 
 for i in range(1, weekly_lookback):
     data['w_avg_pollution-%d' % i] = data['w_avg_pollution'].shift(i*24*7) 
-    data['w_avg_pollution-%d' % i] = data['w_avg_pollution-%d' % i].fillna(data['w_avg_pollution'][0])                                                                           # we don't start at exact 
-                                                                          
+    data['w_avg_pollution-%d' % i] = data['w_avg_pollution-%d' % i].fillna(data['w_avg_pollution'][0])   
+
+monthly_average_pollution = data.groupby(['year', 'month'])['pollution'].transform('mean')
+data['m_avg_pollution']=monthly_average_pollution
+
+for i in range(0, monthly_lookback-1): 
+    prev_month = pd.to_datetime(data['date']).apply(lambda x: x-relativedelta(months=(i+1)))
+    data.insert(1+2*i, 'month-%d_month'%(i+1), prev_month.dt.month) 
+    data.insert(1+2*i+1, 'month-%d_year'%(i+1), prev_month.dt.year) 
+    
+mapa = pd.DataFrame(data.groupby(['year', 'month'])['pollution'].mean().reset_index())
+mapa.rename(columns={'pollution': "tmp"}, inplace=True)
+for i in range(1, monthly_lookback):
+    data = pd.merge(data, mapa, left_on=['month-%d_month'%i, 'month-%d_year'%i], right_on=['month', 'year'], how='left')
+    data.rename(columns={'tmp':'m_avg_pollution-%d'%i}, inplace=True)
+    data.drop(data.columns[-3:-1].values, axis=1, inplace=True)
+    data.drop(['month-%d_month'%i], axis=1, inplace=True)
+    data.drop('month-%d_year'%i, axis=1, inplace=True)
+
+
 
 data.to_csv(output_data_loc)
 
